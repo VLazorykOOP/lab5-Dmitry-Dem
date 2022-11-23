e6218f79040706f32f737514c6764343936edc5c
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2022-11-23 10:54:44 +0200

    Complited Task 2

--- Lab5CSharp/Program.cs
+++ Lab5CSharp/Program.cs
@@ -1,24 +1,60 @@
-﻿using Lab5CSharp;
+﻿
 
 Console.WriteLine("Lab5 C# Demenchuk Dmytro");
-void testTask1()
-{
-    Console.WriteLine("\n\nInsuranceCompany Enter: Name, Address, Classification, Type of Property");
-    InsuranceCompany ins = new InsuranceCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
-    ins.show();
 
-    Console.WriteLine("\n\nPlant Enter: Name, Address, Classification, Owner");
-    Plant plt = new Plant(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
-    plt.show();
+//Lab5CSharp_Task1.Task1.testTask1();
 
-    Console.WriteLine("\n\nOilGasCompany Enter: Name, Address, Classification, Owner, The number pumping stations, The number gas processing plants");
-    OilGasCompany ogc = new OilGasCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
-    ogc.show();
+Lab5CSharp_Task2.Task2.testTask2();
 
-    Console.WriteLine("\n\n");
+GC.Collect();
+
+namespace Lab5CSharp_Task1 
+{ 
+    class Task1
+    {
+        public static void testTask1()
+        {
+            Console.WriteLine("Task 1");
+
+            Console.WriteLine("\n\nInsuranceCompany Enter: Name, Address, Classification, Type of Property");
+            InsuranceCompany ins = new InsuranceCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            ins.show();
+
+            Console.WriteLine("\n\nPlant Enter: Name, Address, Classification, Owner");
+            Plant plt = new Plant(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            plt.show();
+
+            Console.WriteLine("\n\nOilGasCompany Enter: Name, Address, Classification, Owner, The number pumping stations, The number gas processing plants");
+            OilGasCompany ogc = new OilGasCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            ogc.show();
+
+            Console.WriteLine("\n\n");
+        }
+    }
 }
+namespace Lab5CSharp_Task2
+{
+    class Task2
+    {
+        public static void testTask2()
+        {
+            Console.WriteLine("Task 2");
 
+            Console.WriteLine("\n\nInsuranceCompany Enter: Name, Address, Classification, Type of Property");
+            InsuranceCompany ins = new InsuranceCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            ins.show();
 
+            Console.WriteLine("\n\nPlant Enter: Name, Address, Classification, Owner");
+            Plant plt = new Plant(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            plt.show();
 
+            Console.WriteLine("\n\nOilGasCompany Enter: Name, Address, Classification, Owner, The number pumping stations, The number gas processing plants");
+            OilGasCompany ogc = new OilGasCompany(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            ogc.show();
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
 
 
--- Lab5CSharp/Task2.cs
+++ Lab5CSharp/Task2.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab5CSharp_Task1
+namespace Lab5CSharp_Task2
 {
     abstract class Organization
     {
@@ -16,12 +17,30 @@ namespace Lab5CSharp_Task1
             Name = "empty";
             Address = "empty";
             Classification = "empty";
+
+            Console.WriteLine($"Organization Constructor: {this.GetType().Name}");
         }
-        public Organization(string name, string address, string classification)
+        public Organization(string name)
         {
             Name = name;
+
+            Console.WriteLine($"Organization Constructor (name): {this.GetType().Name}");
+        }
+        public Organization(string name, string address) : this(name)
+        {
             Address = address;
+
+            Console.WriteLine($"Organization Constructor (name, address): {this.GetType().Name}");
+        }
+        public Organization(string name, string address, string classification) : this(name, address)
+        {
             Classification = classification;
+
+            Console.WriteLine($"Organization Constructor (name, address, classification): {this.GetType().Name}");
+        }
+        ~Organization()
+        {
+            Console.WriteLine($"Organization Destructor: {this.GetType().Name}");
         }
         public virtual void show() => Console.Write($"{this.GetType().Name}:  Name: {Name}  Address: {Address}  Classification: {Classification}  ");
     }
@@ -31,10 +50,26 @@ namespace Lab5CSharp_Task1
         public InsuranceCompany()
         {
             TypeProperty = "empty";
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public InsuranceCompany(string name) : base(name)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public InsuranceCompany(string name, string address) : base(name, address)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
         }
         public InsuranceCompany(string name, string address, string classification, string typeProperty) : base(name, address, classification)
         {
             TypeProperty = typeProperty;
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        ~InsuranceCompany()
+        {
+            Console.WriteLine($"Destructor: {this.GetType().Name}");
         }
         public new void show()
         {
@@ -48,10 +83,26 @@ namespace Lab5CSharp_Task1
         public Plant()
         {
             Owner = "empty";
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public Plant(string name) : base(name)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public Plant(string name, string address) : base(name, address)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
         }
         public Plant(string name, string address, string classification, string owner) : base(name, address, classification)
         {
             Owner = owner;
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        ~Plant()
+        {
+            Console.WriteLine($"Destructor: {this.GetType().Name}");
         }
         public new void show()
         {
@@ -69,12 +120,28 @@ namespace Lab5CSharp_Task1
             Owner = "empty";
             NumberPumpingStations = "empty";
             NumberGasProcessingPlants = "empty";
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public OilGasCompany(string name) : base(name)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        public OilGasCompany(string name, string address) : base(name, address)
+        {
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
         }
         public OilGasCompany(string name, string address, string classification, string owner, string numberPumpingStations, string numberGasProcessingPlants) : base(name, address, classification)
         {
             Owner = owner;
             NumberPumpingStations = numberPumpingStations;
             NumberGasProcessingPlants = numberGasProcessingPlants;
+
+            Console.WriteLine($"Constructor: {this.GetType().Name}");
+        }
+        ~OilGasCompany()
+        {
+            Console.WriteLine($"Destructor: {this.GetType().Name}");
         }
         public new void show()
         {
